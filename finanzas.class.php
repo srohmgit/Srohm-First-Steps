68cdfb2753a41a027d10dcf6e91206c3bc1a7256
Dev Salbador <devsrhfk@gmail.com>
2025-07-03 17:58:58 -0600

    Mi primer commit

---
+++ finanzas.class.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Clase Finanzas
+ *
+ * Representa la gestión de recursos financieros incluyendo monedas, billetes y mercancías.
+ *
+ * @author Salbador
+ * @version 1.0
+ */
+class Finanzas {
+
+    /**
+     * @var array $monedas Lista de monedas registradas.
+     */
+    private $monedas;
+
+    /**
+     * @var array $billetes Lista de billetes disponibles o gestionados.
+     */
+    private $billetes;
+
+    /**
+     * @var array $mercancias Lista de mercancías asociadas a la gestión financiera.
+     */
+    private $mercancias;
+
+}
+
